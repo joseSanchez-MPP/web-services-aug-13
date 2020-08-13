582415a746f5d7f57eb7ff07697e83ab1666c0da
Jose Sanchez <josersanchez0117@gmail.com>
2020-08-13 13:22:30 -0500

    Initial Commit

---
+++ tvWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tvWeatherApp
+//
+//  Created by jose sanchez on 8/13/20.
+//  Copyright © 2020 jose sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
